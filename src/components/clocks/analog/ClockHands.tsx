99d229619f724e1f0b8be7251f3b2507420a2392
conicuznhm <conicuzn@hotmail.com>
2023-06-10 02:20:13 +0700

    finish analog clock

--- src/components/clocks/analog/ClockHands.tsx
+++ src/components/clocks/analog/ClockHands.tsx
@@ -2,9 +2,9 @@
 import { useEffect, useState } from "react";
 
 export default function ClockHands() {
-  const hands = {
-    hr: "h-[4rem] w-2",
-    min: "h-[8rem] w-1.5",
+  const handSizes = {
+    hr: "h-[5rem] w-2",
+    min: "h-[8rem] w-[0.3rem]",
     sec: "h-[9rem] w-[0.1rem]"
   };
   const handColors = {
@@ -16,31 +16,51 @@ export default function ClockHands() {
   // clock hand movement (will be make to module next)
   const getTime = () => {
     const date = new Date();
+
+    // Get current hr,min,sec
     const hr = date.getHours();
     const min = date.getMinutes();
     const sec = date.getSeconds();
 
-    const hrAngle = hr * 30 + min / 2;
+    // Set angle for hr,min,sec hands
+    const hrAngle = hr * 30 + min * 0.5;
     const minAngle = min * 6;
     const secAngle = sec * 6;
 
     return { hr: hrAngle, min: minAngle, sec: secAngle };
   };
-  const { hr, min, sec } = getTime();
   // clock hand movement (will be make to module next)
+
+  const { hr, min, sec } = getTime();
+  const [hrAngle, setHrAngle] = useState<number>(hr);
+  const [minAngle, setMinAngle] = useState<number>(min);
+  const [secAngle, setSecAngle] = useState<number>(sec);
+
+  useEffect(() => {
+    // Need real time use getTime.key instead of const {key} = getTime()
+    const updateAngle = () => {
+      setHrAngle(getTime().hr);
+      setMinAngle(getTime().min);
+      setSecAngle(getTime().sec);
+    };
+    const interval = setInterval(updateAngle, 1000);
+    return () => clearInterval(interval);
+  }, [hrAngle, minAngle, secAngle]);
 
   return (
     <>
-      <span className="h-2.5 w-2.5 rounded-full bg-black"></span>
+      <span className="h-3 w-3 rounded-full bg-black"></span>
       <span
-        className={`${hands.hr} ${handColors.hr} absolute bottom-44 origin-bottom rounded-full`}
+        className={`${handSizes.hr} ${handColors.hr} absolute bottom-44 origin-bottom rounded-full`}
+        style={{ transform: `rotate(${hrAngle}deg)` }}
       ></span>
       <span
-        className={`${hands.min} ${handColors.min} absolute bottom-44 origin-bottom rounded-full`}
+        className={`${handSizes.min} ${handColors.min} absolute bottom-44 origin-bottom rounded-full`}
+        style={{ transform: `rotate(${minAngle}deg)` }}
       ></span>
       <span
-        className={`${hands.sec} ${handColors.sec} absolute bottom-44 origin-bottom rounded-full`}
-        style={{ transform: "rotate(" + sec + "deg)" }}
+        className={`${handSizes.sec} ${handColors.sec} absolute bottom-44 origin-bottom rounded-full`}
+        style={{ transform: `rotate(${secAngle}deg)` }}
       ></span>
     </>
   );
