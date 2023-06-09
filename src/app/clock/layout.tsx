b05f457f32f60bd4904db7c2d77fbff0610a8603
conicuznhm <conicuzn@hotmail.com>
2023-06-10 03:02:15 +0700

    adjust clock label and clock obj

--- src/app/clock/layout.tsx
+++ src/app/clock/layout.tsx
@@ -4,7 +4,7 @@ interface ClockProps {
 export default function ClockLayout({ children }: ClockProps) {
   return (
     <main className="bg-sky-200 py-5">
-      <section className="flex justify-center align-center">{children}</section>
+      <section className="flex justify-center align-center text-center">{children}</section>
     </main>
   );
 }
--- src/app/clock/page.tsx
+++ src/app/clock/page.tsx
@@ -2,7 +2,7 @@ import { ClockApp } from "@/utils";
 export default function Clock() {
   return (
     <main>
-      <h1>Clock</h1>
+      <h1 className="text-lg mb-4">Analog Clock</h1>
       <ClockApp />
     </main>
   );
